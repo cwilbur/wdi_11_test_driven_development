0b57055fa2782f2cc1244b41797de4ea57c65530
Charlton Wilbur <cwilbur@chromatico.net>
2015-07-01 14:43:43 -0400

    update jshint configuration asynchronously at invocation

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,82 @@
 // http://www.html5rocks.com/en/tutorials/tooling/supercharging-your-gruntfile/
 // discuss how to break up gruntfiles
 
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+
 module.exports = function(grunt) {
 
   require('time-grunt')(grunt);
   require('load-grunt-config')(grunt);
 
+  if (grunt.task.exists('jshint')) {
+
+    // dynamically update our jshint configuration
+
+    grunt.renameTask('jshint', 'jshint-actual');
+    grunt.registerTask('jshint',
+      'Update the jshint config based on package.json and .jshintrc files',
+      function() {
+
+        var done = this.async();
+
+        if (grunt.config('package.jshintConfig')) {
+
+          grunt.config('jshint.options', grunt.config('package.jshintConfig'));
+          return done();
+
+        } else {
+
+          retrieveFileFromClosestDirInPath(path.resolve(), '.jshintrc',
+            function(error, fileContents) {
+              if (error) {
+                return done(error);
+              }
+              if (fileContents) {
+                grunt.config('jshint.options', fileContents);
+              }
+
+              grunt.renameTask('jshint', 'jshint-configure');
+              grunt.renameTask('jshint-actual', 'jshint');
+              grunt.task.run('jshint');
+
+              return done();
+            });
+        }
+
+      });
+  }
+
+};
+
+var retrieveFileFromClosestDirInPath = function(pathName, filename, callback) {
+  var pathComponents = pathName.split(path.sep);
+  if (pathComponents.length && pathComponents[0] === '') {
+    pathComponents[0] = '/';
+  }
+
+  (function findRecurseOrFail(pathComponents, filename, callback) {
+    if (!(pathComponents && pathComponents.length)) {
+      return callback(null, undefined);
+    }
+    var pathString = path.join.apply(path, pathComponents.concat(filename));
+    fs.readFile(pathString, {
+      encoding: 'utf-8'
+    }, function(error, data) {
+
+      if (error && error.code === 'ENOENT') {
+        pathComponents.pop();
+        return findRecurseOrFail(pathComponents, filename, callback);
+      } else if (error) {
+        return callback(error);
+      } else {
+        try {
+          return callback(null, JSON.parse(data));
+        } catch (thrownError) {
+          return callback(thrownError);
+        }
+      }
+    });
+  })(pathComponents, filename, callback);
 };
